6456193a95472f2e625fda3f0f3b863c184d9894
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2020-09-12 19:58:58 +0700

    text color update

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
-const fubuki = document.querySelectorAll('.hide')
+const fubuki = document.querySelectorAll('#fubuki path')
 const names = document.querySelectorAll('#fubuki-text path:not(.inner-box)')
+const innerBox = document.querySelectorAll('.inner-box')
 
 fubuki.forEach((part) => {
     part.style.opacity = 0
 })
 
 names.forEach((name) => {
-    name.setAttribute('stroke', '#000000')
-    name.setAttribute('stroke-width', 1)
+    name.setAttribute('stroke', '#477786')
+    name.setAttribute('stroke-width', 2)
+})
+
+innerBox.forEach((line) => {
+    line.setAttribute('stroke', '#477786')
+    line.setAttribute('stroke-width', 2)
 })
 
 document.querySelectorAll('svg').forEach((svg) => {
 	svg.style.display = 'block';
 });
 
-anime.timeline({
-    // direction: 'alternate',
-    // loop: true,
+var fubukiTl = anime.timeline()
 
-}).add({
+fubukiTl.add({
     targets: fubuki,
     opacity: 1,
-    delay: anime.stagger(5, {
+    delay: anime.stagger(1, {
         from: 'center'
     }),
     complete: function() {
         console.log('Show Fubuki')
     }
 }).add({
+    targets: innerBox,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    complete: function() {
+        console.log('Show Text')
+    }
+}).add({
     targets: names,
     strokeDashoffset: [anime.setDashoffset, 0],
-    fill: '#000',
+    fill: '#D6F3EA',
     duration: 2000,
     easing: 'easeInOutSine',
-    delay: anime.stagger(100),
+    loop: true,
     complete: function() {
         console.log('Show Text')
     }
